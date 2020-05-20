4bf58fcfbcab595b59d2b65c12dfb15af933f4f2
Shailesh Kumar Singh <singhshailesh2295@gmail.com>
2020-05-20 14:18:37 +0530

    [Shailesh] extra space removal in mobile

--- src/Home/CategoryComponent.js
+++ src/Home/CategoryComponent.js
@@ -1,52 +1,38 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-import Layout from '../molecule/Layout'
-import './categoryComponent.css'
-import Title from './Title/Title'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Layout from "../molecule/Layout";
+import "./categoryComponent.css";
+import Title from "./Title/Title";
 
-export default function CategoryComponent(props){
-
-  const[storiesData,setStoriesData] = useState([])
-  const [shouldShowTitle,setShouldShowTitle] = useState(false)
+export default function CategoryComponent(props) {
+  const [storiesData, setStoriesData] = useState([]);
+  const [shouldShowTitle, setShouldShowTitle] = useState(false);
 
   useEffect(() => {
-    async function fetchData(){
+    async function fetchData() {
       const result = await axios(props.url);
-      let stories = result.data.items.filter(item=>item.type==='story');
+      let stories = result.data.items.filter((item) => item.type === "story");
       setStoriesData(stories);
-      setShouldShowTitle(stories.length>0)
+      setShouldShowTitle(stories.length > 0);
     }
     fetchData();
-  },[])
+  }, []);
 
-
-
-  return(
+  return (
     <div>
-      {
-        shouldShowTitle
-        ?
-        <Title name={props.name}/>
-        :
-        null
-      }
+      {shouldShowTitle ? <Title name={props.name} /> : null}
 
-      <div className='slug-container'>
-      {
-        storiesData.slice(0,6)
-        .map(item=>
+      <div className="slug-container">
+        {storiesData.slice(0, 6).map((item) => (
           <div key={item.id}>
-          <Layout
-          imgKey={item.story["hero-image-s3-key"]}
-          headline={item.story.headline}
-          authorName={item.story["author-name"]}
-          />
+            <Layout
+              imgKey={item.story["hero-image-s3-key"]}
+              headline={item.story.headline}
+              authorName={item.story["author-name"]}
+            />
           </div>
-        )
-      }
-
+        ))}
+      </div>
     </div>
-    </div>
-
-  )
+  );
 }

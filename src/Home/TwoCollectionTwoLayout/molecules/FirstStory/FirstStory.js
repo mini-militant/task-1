7d551c22e61fc3bd6e6a531cd3f84618c61d056f
Shailesh Kumar Singh <singhshailesh2295@gmail.com>
2020-05-31 07:28:42 +0530

    [Shailesh]two container layout implementation

--- src/Home/TwoCollectionTwoLayout/molecules/FirstStory/FirstStory.js
+++ src/Home/TwoCollectionTwoLayout/molecules/FirstStory/FirstStory.js
@@ -3,7 +3,11 @@ import "./FirstStory.css";
 export default function FirstStory(props) {
   return (
     <div className="FirstStory-container">
-      <img src={`https://images.assettype.com/${props.imgKey}`} alt="error" />
+      <img
+        className="FirstStory-image"
+        src={`https://images.assettype.com/${props.imgKey}`}
+        alt="error"
+      />
       <p className="FirstStory-headline">{props.headline}</p>
     </div>
   );
